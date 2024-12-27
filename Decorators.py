ab68ee19e308c27847dd1c5b3476ba905ca40c80
kaimul19 <kaimulcock19@gmail.com>
2024-12-26 17:29:41 -0800

    Added TimeMeasure Decorator

---
+++ Decorators.py
@@ -0,0 +1,18 @@
+from time import perf_counter_ns
+from functools import wraps
+
+# TimeMeasure taken from another of my Git Repo's - https://github.com/JCatton/KJRJMCMCMC/tree/Test-Inclination-Bounds
+def TimeMeasure(func):
+    """Measure performance of a function"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = perf_counter_ns()
+        func_return = func(*args, **kwargs)
+        finish_time = perf_counter_ns()
+        print(
+            f"Function: {func.__name__} took {(finish_time - start_time)*1e-6:.2f} ms"
+        )
+        return func_return
+
+    return wrapper
