a258d4be40cf039776a157f9807b5cd226714fa8
kaimul19 <kaimulcock19@gmail.com>
2025-07-02 05:42:37 -0700

    add sinusoid generator

--- Injection_Flavours.py
+++ Injection_Flavours.py
@@ -82,3 +82,51 @@ def add_linear(cadence, signal_params, true_or_false, bin_width=4096):
         raise ValueError("true_or_false must be either True or False")
 
     return cadence
+
+
+
+def add_sinusoid(cadence, signal_params, true_or_false, bin_width= 4096):
+
+        # Generate initial parameters
+    start, drift, drift_factor, x_starts = initial_params_generator(bin_width)
+
+    # Indices of the cadences where the signal will be injected
+    indexes = [0, 2, 4]  # Inject signals into these cadences
+    intensity = cadence[0].get_intensity(snr=60)  # Signal intensity based on the first cadence
+    
+    # Add the signal to the specified cadences
+    for i in indexes:
+        cadence[i].add_signal(
+            stg.constant_path(
+                f_start=cadence[i].get_frequency(index=int(x_starts[i])),
+                drift_rate=drift * drift_factor * u.Hz / u.s,
+            ),
+            stg.constant_t_profile(level=intensity),
+            stg.box_f_profile(width=80 * cadence[i].df * u.Hz),
+            stg.constant_bp_profile(level=1),
+        )
+
+        cadence[i].add_signal(stg.sine_path(f_start=cadence[i].get_frequency(index=int(x_starts[i])),
+                                        drift_rate=drift * drift_factor * u.Hz / u.s,
+                                        period=100*u.s,
+                                        amplitude=200*u.Hz),
+                          stg.constant_t_profile(level=intensity),
+                          stg.box_f_profile(width=80*cadence[i].df * u.Hz),
+                          stg.constant_bp_profile(level=1))
+    # Add a false signal to the remaining cadences if true_or_false is False
+    if not true_or_false:
+        false_indexes = [1, 3, 5]  # Inject false signals into these cadences
+        for i in false_indexes:
+        cadence[i].add_signal(stg.sine_path(f_start=cadence[i].get_frequency(index=int(x_starts[i])),
+                                        drift_rate=drift * drift_factor * u.Hz / u.s,
+                                        period=100*u.s,
+                                        amplitude=200*u.Hz),
+                          stg.constant_t_profile(level=intensity),
+                          stg.box_f_profile(width=80*cadence[i].df * u.Hz),
+                          stg.constant_bp_profile(level=1))
+
+    elif true_or_false != True:
+        raise ValueError("true_or_false must be either True or False")
+
+    return cadence
+    
