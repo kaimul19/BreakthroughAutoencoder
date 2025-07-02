7b7a4b2b4693fc3eb715f45cb997ab901400d68d
kaimul19 <kaimulcock19@gmail.com>
2025-07-02 05:42:28 -0700

    add initial param generator funciton, allows jitting to accelerate + is repeated so cleaner

--- Injection_Flavours.py
+++ Injection_Flavours.py
@@ -2,10 +2,34 @@ import numpy as np              # np.array, arithmetic
 import random as rd             # rd.random, rd.choice
 import setigen as stg           # stg.constant_path, stg.Frame, …
 from astropy import units as u  # u.Hz, u.s  (Astropy units)
+from numba import jit          # JIT compilation for performance
 
 # Optional – only if you keep the decorator line
 from Decorators import TimeMeasure
 
+
+@jit
+def initial_params_generator(bin_width=4096):
+    # Random starting frequency index
+    start = 1 + np.random.randint(0, bin_width - 1)
+    rand_multiplier = rd.choice([-1, 1])  # Randomly choose the drift direction
+    y_points = np.array((0, 15, 31, 47, 63, 79)) # Y-points for the 6 cadences
+
+    # Calculate max slope based on the starting frequency
+    if rand_multiplier == -1:
+        max_slope = -(96 / start) * (18.25361108/2.7939677238464355)
+    else:
+        space_to_end = bin_width - start
+        max_slope = (96 / space_to_end) * (18.25361108/2.7939677238464355)
+
+    # Generate slope and drift
+    drift = (1 / max_slope) * rd.random()
+    drift_factor = 2.7939677238464355/18.25361108
+
+    # Calculate the x-coordinates for the 6 cadences
+    x_starts = y_points * drift + start
+
+    return start, drift, drift_factor, x_starts
 
 # @TimeMeasure
 def add_linear(cadence, signal_params, true_or_false, bin_width=4096):
@@ -21,26 +45,8 @@ def add_linear(cadence, signal_params, true_or_false, bin_width=4096):
     Returns:
     - Updated OrderedCadence object.
     """
-
-    # Random starting frequency index
-    start = int(rd.random() * (bin_width - 1)) + 1
-    RandMultiplier = rd.choice([-1, 1])  # Randomly choose the drift direction
-    y_points = np.array((0, 15, 31, 47, 63, 79)) # Y-points for the 6 cadences
-
-    # Calculate max slope based on the starting frequency
-    if RandMultiplier == -1:
-        MaxSlope = -(96 / start) * (18.25361108/2.7939677238464355)
-    else:
-        spaceToEnd = bin_width - start
-        MaxSlope = (96 / spaceToEnd) * (18.25361108/2.7939677238464355)
-
-    # Generate slope and drift
-    drift = (1 / MaxSlope) * rd.random()
-    drift_factor = 2.7939677238464355/18.25361108
-
-    # Calculate the x-coordinates for the 6 cadences
-    x_starts = y_points * drift + start
-
+    # Generate initial parameters
+    start, drift, drift_factor, x_starts = initial_params_generator(bin_width)
 
     # Indices of the cadences where the signal will be injected
     indexes = [0, 2, 4]  # Inject signals into these cadences
