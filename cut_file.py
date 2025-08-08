b029847e3d1ba6f0d3cd9857783b5cdcdb2b373d
kaimul19 <kaimulcock19@gmail.com>
2025-08-08 06:15:58 -0700

    automate naming in cut_file

--- cut_file.py
+++ cut_file.py
@@ -2,22 +2,23 @@ import os
 import numpy as np
 
 # Load the full original memmap
-file_name = 'generated_data/background_seperated_raw_data.npy'
+file_name = 'generated_data/background_for_signals_only_40k_raw_data.npy'
 data_shape_name = file_name.replace('.npy', '_shape.npy')
 data_shape = np.load(data_shape_name)
 data_shape = tuple(int(dim) for dim in data_shape)
 
 data = np.memmap(file_name, dtype='float32', mode='r+', shape=data_shape)
 # Slice the last 2000 entries
-data_subset = data[0:10000].copy()
+num_data = 2000
+data_subset = data[0:num_data].copy()
 
 # Path to overwrite the original memmap file
-output_path = 'generated_data/first_10000_raw_data.npy'  # or whatever the original file is
+output_path = f'generated_data/first_{str(num_data)}_raw_data.npy'  # or whatever the original file is
 
 # Overwrite the file with new memmap content
 memmap_subset = np.memmap(output_path, dtype='float32', mode='w+', shape=data_subset.shape)
 memmap_subset[:] = data_subset[:]
 memmap_subset.flush()
 
-shape_path = 'generated_data/first_10000_raw_data_shape.npy'
+shape_path = f'generated_data/first_{str(num_data)}_raw_data_shape.npy'
 np.save(shape_path, np.array(data_subset.shape, dtype=np.int64))
