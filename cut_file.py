6e1a71996e5d0b3443207d249dfc0648a10917b0
kaimul19 <kaimulcock19@gmail.com>
2025-08-08 05:49:53 -0700

    running changes ofr cut_file

--- cut_file.py
+++ cut_file.py
@@ -2,21 +2,22 @@ import os
 import numpy as np
 
 # Load the full original memmap
-data_shape = np.load('generated_data/background_data_shape.npy')
+file_name = 'generated_data/background_seperated_raw_data.npy'
+data_shape_name = file_name.replace('.npy', '_shape.npy')
+data_shape = np.load(data_shape_name)
 data_shape = tuple(int(dim) for dim in data_shape)
-file_name = 'generated_data/background_data.npy'
-data = np.memmap(file_name, dtype='float32', mode='r+', shape=data_shape)
 
+data = np.memmap(file_name, dtype='float32', mode='r+', shape=data_shape)
 # Slice the last 2000 entries
-data_subset = data[0:2000].copy()
+data_subset = data[0:10000].copy()
 
 # Path to overwrite the original memmap file
-output_path = 'generated_data/first_2000_entries_memmap_seperated_raw_data.npy'  # or whatever the original file is
+output_path = 'generated_data/first_10000_raw_data.npy'  # or whatever the original file is
 
 # Overwrite the file with new memmap content
 memmap_subset = np.memmap(output_path, dtype='float32', mode='w+', shape=data_subset.shape)
 memmap_subset[:] = data_subset[:]
 memmap_subset.flush()
 
-shape_path = 'generated_data/first_2000_shape.npy'
+shape_path = 'generated_data/first_10000_raw_data_shape.npy'
 np.save(shape_path, np.array(data_subset.shape, dtype=np.int64))
